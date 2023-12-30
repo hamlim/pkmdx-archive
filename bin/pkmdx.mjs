a3fbd19218c23ce407941a3109945eeab99fcc36
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-12-30 07:18:42 -0800

    WIP

---
+++ bin/pkmdx.mjs
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import { exec } from "node:child_process";
+
+let port = process.argv.find(flag => flag.includes('--port') || flag.includes('-p')).split(/=|\s/)[1] || 8888;
+
+exec(`bun run start --port ${port}`, (err, stdout) => {
+  if (err)  {
+    console.error(err)
+    process.exit(1);
+  }
+  console.log('Launched server on port', port)
+  console.log(`Launching browser at http://127.0.0.1:${port}`)
+  exec(`open http://127.0.0.1:${port}`)
+
+})
+
